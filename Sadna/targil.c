05f54bffd06485d0f08ae27b054fefcec112d828
Dolev <dolevm@gmail.com>
2021-05-01 23:47:16 +0300

    ex1

--- Sadna/targil.c
+++ Sadna/targil.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <time.h>
+#include<math.h>
 
 /* Function declarations */
 
@@ -14,13 +16,15 @@ void Ex7();
 
 /* Declarations of other functions */
 
+unsigned int * powerArray(int num);
+
 /* ------------------------------- */
 
 int main()
 {
 	int select = 0, i, all_Ex_in_loop = 0;
 	printf("Run menu once or cyclically?\n(Once - enter 0, cyclically - enter other number) ");
-	if (scanf("%d", &all_Ex_in_loop) == 1)
+	if (scanf_s("%d", &all_Ex_in_loop) == 1)
 		do
 		{
 			for (i = 1; i <= 7; i++)
@@ -29,7 +33,7 @@ int main()
 			do {
 				select = 0;
 				printf("please select 0-7 : ");
-				scanf("%d", &select);
+				scanf_s("%d", &select);
 			} while ((select < 0) || (select > 7));
 			switch (select)
 			{
@@ -47,5 +51,53 @@ int main()
 
 
 /* Function definitions */
+void Ex1()
+{
+	unsigned int *p, num, i;
+	printf("enter a number greater than 0: ");
+	scanf_s("%d", &num);
+	p = powerArray(num);
+	for (i = 0; i < num; i++)
+	{
+		printf("%d, ", p[i]);
+	}
+}
+void Ex2()
+{
 
+}
+void Ex3()
+{
+
+}
+void Ex4()
+{
+
+}
+void Ex5()
+{
+
+}
+void Ex6()
+{
+
+}
+void Ex7()
+{
+
+}
 /* ------------------- */
+
+/* Declarations of other functions */
+
+unsigned int * powerArray(int num)
+{
+	unsigned int * arr;
+	int i;
+	arr = (int *)malloc(sizeof(int *)* num);
+	for (i = 0; i < num; i++)
+	{
+		arr[i] = pow(2.0, i % 32);
+	}
+	return arr;
+}
