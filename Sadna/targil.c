6ab19425dd686d92eb1749a40103ef59c9ffeab5
Dolev <dolevm@gmail.com>
2021-05-04 23:55:49 +0300

    ex7

--- Sadna/targil.c
+++ Sadna/targil.c
@@ -4,6 +4,24 @@
 #include <time.h>
 #include<math.h>
 
+/* Structs */
+typedef struct test
+{
+	int data;
+}Test;
+
+typedef struct node
+{
+	int data;
+	struct node *next;
+}Node;
+
+typedef struct char_node
+{
+	char data;
+	struct node *next;
+}Char_Node;
+
 /* Function declarations */
 
 void Ex1();
@@ -17,13 +35,15 @@ void Ex7();
 /* Declarations of other functions */
 
 unsigned int * powerArray(int num);
-
+Node * createNode(int);
+Node * addToFirst(Node *, int);
+Node * addToLast(Node *, int);
+Char_Node * createCharNode(char);
+Char_Node * addCharToFirst(Char_Node *, char);
+Char_Node * addCharToLast(Char_Node *, char);
 /* ------------------------------- */
 
-typedef struct test
-{
-	int data;
-}Test;
+
 
 int main()
 {
@@ -89,7 +109,19 @@ void Ex6()
 }
 void Ex7()
 {
-
+	int num, i;
+	char data;
+	printf("Enter a number of char: ");
+	scanf_s("%d", &num);
+	Char_Node * list = NULL;
+	printf("Enter a char: ");
+	list = addCharToFirst(list, scanf_s("%c", &data));
+	for (i = 0; i < num-1; i++)
+	{
+		printf("Enter a char: ");
+		list = addCharToLast(list, scanf_s("%c", &data));
+	}
+	disassemblyList(list);
 }
 /* ------------------- */
 
@@ -105,4 +137,73 @@ unsigned int * powerArray(int num)
 		arr[i] = pow(2.0, i % 32);
 	}
 	return arr;
+}
+
+Node * createNode(int data)
+{
+	Node *temp = (Node *)malloc(sizeof(Node));
+	if (temp == NULL)
+		return;
+	temp->data = data;
+	temp->next = NULL;
+	return temp;
+}
+
+Node * addToFirst(Node * head, int data)
+{
+	Node *temp = createNode(data);
+	temp->next = head;
+	return temp;
+}
+
+Node * addToLast(Node *head, int data)
+{
+	Node *temp = createNode(data);
+	Node *p = head;
+	if (head == NULL)
+		return temp;
+	while (p->next != NULL)
+		p = p->next;
+	p->next = temp;
+	return head;
+}
+
+Char_Node * createCharNode(char data)
+{
+	Char_Node *temp = (Char_Node *)malloc(sizeof(Char_Node));
+	if (temp == NULL)
+		return;
+	temp->data = data;
+	temp->next = NULL;
+	return temp;
+}
+
+Char_Node * addCharToFirst(Char_Node * head, char data)
+{
+	Char_Node *temp = createNode(data);
+	temp->next = head;
+	return temp;
+}
+
+Char_Node * addToLast(Char_Node *head, char data)
+{
+	Char_Node *temp = createNode(data);
+	Char_Node *p = head;
+	if (head == NULL)
+		return temp;
+	while (p->next != NULL)
+		p = p->next;
+	p->next = temp;
+	return head;
+}
+
+void disassemblyList(Char_Node * list)
+{
+	while (list->next != NULL)
+	{
+		if (list->data >= 'a' || list->data <= 'z')
+		{
+
+		}
+	}
 }
